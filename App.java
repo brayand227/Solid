00b13efeaf7e67032898ee1d1e1edd939ef96e3c
brayand227 <brayancalle227@gmail.com>
2024-02-20 12:29:26 -0500

    hola mundo

--- App.java
+++ App.java
@@ -7,6 +7,8 @@ package solid.java;
 public class App {
     public static void main(String[] args) {
         System.out.println("Helol");
+        System.out.println("-------------");
+
         System.out.println("-------------------");
     }
 }
